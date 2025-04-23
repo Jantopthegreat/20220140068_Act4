2f466fb381263af0b94f941a39fd343983c91891
Jantopthegreat <fauzangriya@gmail.com>
2025-04-23 22:59:37 +0700

    membuat widget untuk detail order page

--- lib/presentation/orderDetail_page.dart
+++ lib/presentation/orderDetail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:orderpage/presentation/home_page.dart';
 
 class DetailOrderPage extends StatelessWidget {
 
@@ -22,6 +23,32 @@ final String makanan;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Order Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food QTY Order: $jumlahMakanan'),
+            Text('Drink QTY Order: $jumlahMinuman'),
+            Text('Total Price: $totalHarga'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false,
+                );
+              },
+              child: Text('Finish Order'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
