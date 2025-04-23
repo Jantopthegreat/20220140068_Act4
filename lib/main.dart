ea06053f19a04d9c867be5af2d5d97b65e7ec824
Jantopthegreat <fauzangriya@gmail.com>
2025-04-23 23:09:09 +0700

    mendaftarkan routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:orderpage/presentation/login_page.dart';
+import 'package:orderpage/presentation/register_page.dart';
+import 'package:orderpage/presentation/home_page.dart';
+import 'package:orderpage/presentation/order_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,93 +19,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-  
-  
-
-  
-  
-  
-  
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      
-      
-      
-      
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    
-    
-    
-    
-    
-    
-    return Scaffold(
-      appBar: AppBar(
-        
-        
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        
-        child: Column(
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/home': (context) => const HomePage(),
+        '/order': (context) => const OrderPage(),
+      }
     );
   }
 }
