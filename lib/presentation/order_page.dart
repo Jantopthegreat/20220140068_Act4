5a91757983db41830891db5687b64574abb5b4ed
Jantopthegreat <fauzangriya@gmail.com>
2025-04-23 22:18:17 +0700

    membuat makanan dan minuman controller

---
+++ lib/presentation/order_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+} 
+
+class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
