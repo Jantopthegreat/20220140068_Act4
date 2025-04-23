d391e7048ebbd51cfa1efa7f4d8466e4a660b1a2
Jantopthegreat <fauzangriya@gmail.com>
2025-04-23 22:46:57 +0700

    membuat properti makanan minuman dll

---
+++ lib/presentation/orderDetail_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DetailOrderPage extends StatelessWidget {
+
+final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+
+ 
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
